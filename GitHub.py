a2729d06094fd4ec7197be1464c260c180fbcbe6
akumfisher <akumfisher@gmail.com>
2026-05-29 15:19:50 +0100

    Added repo validation and empty state handling

--- GitHub.py
+++ GitHub.py
@@ -1,6 +1,7 @@
 import json
 import os
 from colorama import Fore, Style
+import requests
 
 FILE = "repos.json"
 
@@ -10,6 +11,22 @@ def load():
         return []
     with open(FILE, "r") as f:
         return json.load(f)
+        
+def url_checker(url):
+    headers = {"User-Agent": "Mozilla/5.0"}
+
+    try:
+        response = requests.get(
+            url,
+            headers=headers,
+            timeout=5,
+            allow_redirects=True
+        )
+
+        return response.status_code == 200
+
+    except requests.RequestException:
+        return False
 
 
 def save(data):
@@ -20,9 +37,13 @@ def save(data):
 def add_repo():
     repos = load()
     url = input(Fore.BLUE + "Repository URL: ")
-    repos.append(url)
-    save(repos)
-    print(Fore.GREEN + "Repository has been saved." + Style.RESET_ALL)
+    if url_checker(url):
+        repos.append(url)
+        save(repos)
+        print(Fore.GREEN + "Repository has been saved." + Style.RESET_ALL)
+    else:
+        print("Invalid repository link")
+      
 
 
 def list_repos():
@@ -46,10 +67,17 @@ def update_repo():
 def delete_repo():
     repos = load()
     list_repos()
+    
+    if repos == []:
+        print(Fore.YELLOW + "No repository was found!" + Style.RESET_ALL)
+        not_empty = False
+    else:
+        not_empty = True
 
-    while True:
+    while not_empty:
         try:
             n = int(input(Fore.BLUE + "Repository number: "))
+            
             repo_range = range(1, len(repos)+1)
 
             if n in repo_range:
